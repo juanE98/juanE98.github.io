f842999a1cfb2c660b3233d05c6ed76d58f10c0e
Juan Espares <juan.espares@dyedurham.com>
2024-08-12 22:41:54 +1000

    feat: About me page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {CommonModule} from "@angular/common";
 import {HeaderComponent} from "./header/header.component";
 import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, HeaderComponent, RouterOutlet, CommonModule],
+  imports: [HomeComponent, HeaderComponent, RouterOutlet, CommonModule, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
