fe472e1d800e0a509ec5a948166646836b0fd529
Juan Espares <juan.espares@dyedurham.com>
2024-08-13 23:00:43 +1000

    feat: Carousel for tech stack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import {CommonModule} from "@angular/common";
 import {HeaderComponent} from "./header/header.component";
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
+import {ImageCarouselComponent} from "./image-carousel/image-carousel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, HeaderComponent, RouterOutlet, CommonModule, AboutComponent],
+  imports: [HomeComponent, HeaderComponent, RouterOutlet, CommonModule, AboutComponent, ImageCarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/image-carousel/image-carousel.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {NgForOf, NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-image-carousel',
+  standalone: true,
+  imports: [
+    NgForOf,
+    NgOptimizedImage
+  ],
+  templateUrl: './image-carousel.component.html',
+  styleUrl: './image-carousel.component.scss'
+})
+export class ImageCarouselComponent implements OnInit {
+  icons: string[] = [];
+
+  ngOnInit() {
+    this.icons = this.generateIconPaths(5, 20);
+    this.icons = [...this.icons, ...this.icons];
+    console.log(this.icons);
+
+  }
+
+  generateIconPaths(start: number, end: number): string[] {
+    const paths: string[] = [];
+    for (let i = start; i <= end; i++) {
+      paths.push(`/assets/icons/${i}.svg`);
+    }
+    return paths;
+  }
+}
