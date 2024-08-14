e8b17121e9ecf65bf7072cdd19fc4657da4ce901
Juan Espares <juan.espares@dyedurham.com>
2024-08-14 22:45:02 +1000

    feat: about me scrolling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
-import {CommonModule} from "@angular/common";
-import {HeaderComponent} from "./header/header.component";
-import {HomeComponent} from "./home/home.component";
-import {AboutComponent} from "./about/about.component";
-import {ImageCarouselComponent} from "./image-carousel/image-carousel.component";
-import {TimelineComponent} from "./timeline/timeline.component";
-import {FooterComponent} from "./footer/footer.component";
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ImageCarouselComponent } from './image-carousel/image-carousel.component';
+import { TimelineComponent } from './timeline/timeline.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, HeaderComponent, RouterOutlet, CommonModule, AboutComponent, TimelineComponent,
-    ImageCarouselComponent, FooterComponent],
+  imports: [
+    HomeComponent,
+    HeaderComponent,
+    RouterOutlet,
+    CommonModule,
+    AboutComponent,
+    TimelineComponent,
+    ImageCarouselComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'website-portfolio';
+
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
+
+  ngOnInit() {
+    this.activatedRoute.fragment.subscribe((fragment: string | null) => {
+      if (fragment) this.jumpToSection(fragment);
+    });
+  }
+
+  jumpToSection(section: string | null) {
+    if (section) document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (window.scrollY === 0) {
+      this.router.navigate([], { fragment: '' });
+    }
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  menuValue:boolean = false;
-  menu_icon:string = "bi bi-list";
-  openMenu(){
+  menuValue: boolean = false;
+  menu_icon: string = "bi bi-list";
+
+  openMenu() {
     this.menuValue = !this.menuValue;
     this.menu_icon = this.menuValue ? "bi bi-x" : "bi bi-list";
   }
-  closeMenu(){
+
+  closeMenu() {
     this.menuValue = false;
     this.menu_icon = "bi bi-list";
+  }
+
+  constructor(private router: Router) {}
+
+  goToPart(fragment: string) {
+    this.router.navigate(['/'], { fragment });
   }
 }
