fa4e586f18d6265ca6b6226ca842434193ae192d
Juan Espares <juan.espares@dyedurham.com>
2024-08-18 09:51:20 +1000

    fix: mobile responsiveness with iPhone 11 scrolling with guided arrow from home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 
 @Component({
@@ -11,5 +11,22 @@ import {NgOptimizedImage} from "@angular/common";
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isAboutInView = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const aboutSection = document.getElementById('about');
+    if (aboutSection) {
+      const rect = aboutSection.getBoundingClientRect();
+      this.isAboutInView = rect.top <= window.innerHeight && rect.bottom >= 0;
+    }
+  }
+
+  scrollToAbout() {
+    const aboutSection = document.getElementById('about');
+    if (aboutSection) {
+      aboutSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 
 }
