3f0def7c43f602533ae0c41b2b038a262a032218
Juan Espares <juloespares@yahoo.com.au>
2025-06-01 20:17:44 +1000

    fix: updated text

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -13,12 +13,13 @@ import {ScrollVisibilityDirective} from "../scroll-visibility.directive";
 })
 export class TimelineComponent {
   readonly events: ReadonlyArray<[string, string, string]> = [
-    ['Pharmacy Student', 'Calanna Terrywhite - 2018', 'rural pharmacy student placement'],
-    ['Bachelor of Pharmaceutics and Therapeutic Science', 'University of Queensland - 2018', ''],
-    ['Pharmacy Assistant', 'Chemist Warehouse - 2019', 'Worked in a fast paced environment where time management and communication / customer support skills were vital.'],
-    ['Bachelor of Computer Science', 'University of Queensland - 2022', ''],
-    ['Scriptsoft', 'Software Developer - 2023', 'Monolithic .NET framework applications for pharmaceutical and small business software.'],
-    ['Dye and Durham', 'Junior Software Engineer - Current', 'Primarily .NET microservices with Angular frontend deployed through kubernetes in GCP.'],
+    ['Pharmacy Student', 'Calanna Terrywhite | 2018', 'rural pharmacy student placement'],
+    ['Bachelor of Pharmaceutics and Therapeutic Science', 'University of Queensland | 2018', ''],
+    ['Pharmacy Assistant', 'Chemist Warehouse | 2019', 'Worked in a fast paced environment where time management and communication / customer support skills were vital.'],
+    ['Bachelor of Computer Science', 'University of Queensland | 2022', ''],
+    ['Scriptsoft', 'Software Developer | 2022 - 2023', 'Monolithic .NET framework applications for pharmaceutical and small business software.'],
+    ['Dye and Durham', 'Junior Software Engineer | 2024 - 2025', 'Primarily .NET microservices with Angular frontend deployed through kubernetes in GCP.'],
+    ['Contal Services', 'Backend Software Engineer - Current', 'Java backend with Spring Boot']
   ];
 
   @HostListener('window:scroll', [])
