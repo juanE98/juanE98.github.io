4df2017ba52b1fcd0a67e00c5e0a56c1ad7a712b
Juan Espares <juloespares@yahoo.com.au>
2025-06-14 06:50:32 +1000

    fix:  disable transition animations on mobile and improve readability

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -17,7 +17,7 @@ export class TimelineComponent {
     ['Bachelor of Pharmaceutics and Therapeutic Science', 'University of Queensland | 2016 - 2018', ''],
     ['Pharmacy Assistant', 'Chemist Warehouse | 2017 - 2019', 'Worked in a fast paced environment where time management and communication / customer support skills were vital.'],
     ['Bachelor of Computer Science', 'University of Queensland | 2019 - 2022', ''],
-    ['Scriptsoft', 'Software Developer | 2022 - 2023', 'Monolithic .NET framework applications for pharmaceutical and small business software.'],
+    ['Scriptsoft', 'Software Developer | 2022 - 2023', 'Monolithic .NET framework applications for pharmaceutical and small business software. Worked part time during my uni days before transitioning to full time upon graduation.'],
     ['Dye and Durham', 'Junior Software Engineer | 2023 - 2024', 'Primarily .NET microservices with Angular frontend deployed through kubernetes in GCP.'],
     ['Contal Services', 'Backend Software Engineer - Current', 'Java backend with Spring Boot']
   ];
